9a580163fe41128de559fda2cb52adb8169d57d0
martinacaramaschi <martina.caramaschi@studio.unibo.it>
2020-08-01 19:01:51 +0200

    stylistic improvemnts of comments

--- call_functions.py
+++ call_functions.py
@@ -2,12 +2,19 @@
 
 from functions import print_ciao, to_lower, read_dataset_and_create_df
 
+#test print_ciao() function
 print_ciao()
 
+#test to_lower() function
 to_lower("hELLO World!")
 
+#test read_dataset_and_create_df() function
 name_dataset = 'database_TitlesAndClusters.xlsx'
 dataframe = read_dataset_and_create_df(name_dataset)
 
-df_columns= dataframe.columns
-print(df_columns)
+dataframe_columns = dataframe.columns
+print(dataframe_columns)
+
+dataframe_shape = dataframe.shape[0]
+dataframe_n_columns = dataframe.shape[1]
+print(dataframe_shape, dataframe_n_columns)
--- functions.py
+++ functions.py
@@ -4,7 +4,7 @@ def print_ciao():
     print("ciao!")
 
 
-#lowercase function: to_lower is the function that give me all the phrases written in uppercase
+#to_lower function: it transforms all input string to lower case
 def to_lower(string):
     try:
         lower_case = ""
@@ -18,18 +18,22 @@ def to_lower(string):
     except:
         print("Not valid input! must be a string!")
 
+#read_dataset_and_create_df function: it read an excel dataset and 
+    #returns a dataframe containing the dataset's informations
 
+#the input needed is the dataset's name (e.g. name_dataset = "name_dataset.xlsx")
 def read_dataset_and_create_df(name_dataset):
     import pandas as pd
 
-    #read the dataset. ET means ESERAtable. This table that contains titles of articles presented during ESERA confrence 2019 as oral presentations
+    #we read and store the dataset's information in dataset
     dataset = pd.read_excel(name_dataset, )
     dataset = pd.read_excel(name_dataset)
 
-    # dfET means dataframeESERAtable and it's the dataframe that i will manage 
+    #than we create the dataframe containing the dataset's infos
     dataframe = dataset.copy()
 
-    #show the index of the dataframe's columns
-    #print(dataframe.columns)
+    #print "okay!" to check all has gone well
     print("okay!")
+    
+    #the functions return the dataframe
     return(dataframe)
