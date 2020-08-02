f9d4c7a7bc9cdb028f4a61a0a6fb2db367f552b4
martinacaramaschi <martina.caramaschi@studio.unibo.it>
2020-08-02 18:48:11 +0200

    stylistic improvemnts of comments; call the preprocessing_titles function

--- call_functions.py
+++ call_functions.py
@@ -1,20 +1,28 @@
 # -*- coding: utf-8 -*-
 
-from functions import print_ciao, to_lower, read_dataset_and_create_df
+from functions import print_ciao, to_lower, read_dataset_and_create_df, \
+                         preprocessing_titles
 
-#test print_ciao() function
+# Testing print_ciao() function
 print_ciao()
 
-#test to_lower() function
+# Testing to_lower(string) function
 to_lower("hELLO World!")
 
-#test read_dataset_and_create_df() function
+# Testing read_dataset_and_create_df() function
 name_dataset = 'database_TitlesAndClusters.xlsx'
 dataframe = read_dataset_and_create_df(name_dataset)
 
 dataframe_columns = dataframe.columns
 print(dataframe_columns)
+print(dataframe_columns[0])
 
-dataframe_shape = dataframe.shape[0]
+titles = dataframe[dataframe_columns[0]]
+print(titles)
+dataframe_n_rows = dataframe.shape[0]
 dataframe_n_columns = dataframe.shape[1]
-print(dataframe_shape, dataframe_n_columns)
+print(dataframe_n_rows, dataframe_n_columns)
+
+# Testing preprocessing_titles(titles, number_of_rows) function
+lower_titles = preprocessing_titles(titles, dataframe_n_rows)
+len(lower_titles)
