6d84cd30d1828e29de0e09607f1de8a6bce6cd25
martinacaramaschi <martina.caramaschi@studio.unibo.it>
2020-08-03 16:30:25 +0200

    i have tested clean_str and preprocessing_titles functions

--- call_functions.py
+++ call_functions.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
 
 from functions import print_ciao, to_lower, read_dataset_and_create_df, \
-                         preprocessing_titles
+                         clean_str
+from preprocessing import preprocessing_titles
 
 # Testing print_ciao() function
 print_ciao()
 
 # Testing to_lower(string) function
 to_lower("hELLO World!")
+
+string = ["hello Wolrd!", "CARAINBIII"] 
+for element in string:
+    to_lower(element)
 
 # Testing read_dataset_and_create_df() function
 name_dataset = 'database_TitlesAndClusters.xlsx'
@@ -18,11 +23,16 @@ print(dataframe_columns)
 print(dataframe_columns[0])
 
 titles = dataframe[dataframe_columns[0]]
-print(titles)
+  
 dataframe_n_rows = dataframe.shape[0]
 dataframe_n_columns = dataframe.shape[1]
 print(dataframe_n_rows, dataframe_n_columns)
 
+# Testing clean_str(string) function
+clean_str("i don't know. maybe i'll do.")
+
 # Testing preprocessing_titles(titles, number_of_rows) function
-lower_titles = preprocessing_titles(titles, dataframe_n_rows)
-len(lower_titles)
+lower_and_cleaned_titles = []
+lower_and_cleaned_titles = preprocessing_titles(titles)
+len(lower_and_cleaned_titles)
+print(lower_and_cleaned_titles)
