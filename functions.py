b0ba81b85fb6a6e5c1fcc9b0c923f30be1da8e2a
martinacaramaschi <martina.caramaschi@studio.unibo.it>
2020-08-03 16:29:15 +0200

    added clean_str function to remove some unuseful charachters from titles

--- functions.py
+++ functions.py
@@ -4,6 +4,17 @@
 def print_ciao():
     print("ciao!")
 
+# read_dataset_and_create_df function: it read an excel dataset and returns a dataframe containing the dataset's informations
+# As input the dataset's name (e.g. name_dataset = "name_dataset.xlsx") is needed
+def read_dataset_and_create_df(name_dataset):
+    import pandas as pd
+    # It reads and stores the dataset's information in dataset
+    dataset = pd.read_excel(name_dataset, )
+    dataset = pd.read_excel(name_dataset)
+    # It creates the dataframe containing the dataset's infos
+    dataframe = dataset.copy() 
+    print("okay!")    # It prints "okay!" to check all has gone well
+    return(dataframe)
 
 # to_lower function: it transforms all input string to lower case
 # As input, a string is needed 
@@ -16,30 +27,21 @@ def to_lower(string):
                 new_ascii = location + ord('a')
                 character = chr(new_ascii)
             lower_case = lower_case + character
-        print(lower_case)
+        #print(lower_case)
     except:
         print("Not valid input! must be a string!")
-
-# preprocessing_titles function: it preprocess the titles before the word2vec training
-# As input the list of titles, the lenght of the list are needed
-def preprocessing_titles(titles, number_of_rows):
-    titles_lowercase = []
-    for i in range(0,number_of_rows):
-        titles_lowercase.append(to_lower(titles[i]))
-    titles = titles_lowercase
-    return(titles)
-
-
-# read_dataset_and_create_df function: it read an excel dataset and returns a dataframe containing the dataset's informations
-# As input the dataset's name (e.g. name_dataset = "name_dataset.xlsx") is needed
-def read_dataset_and_create_df(name_dataset):
-    import pandas as pd
-    # It reads and stores the dataset's information in dataset
-    dataset = pd.read_excel(name_dataset, )
-    dataset = pd.read_excel(name_dataset)
-    # It creates the dataframe containing the dataset's infos
-    dataframe = dataset.copy()
-    # It prints "okay!" to check all has gone well
-    print("okay!")
+    return(lower_case)
     
-    return(dataframe)
+    
+# clean_str function: it removes or substitutes some string (e.g. 'll is removed and n't become not)
+# As input a string is needed
+def clean_str(string):
+    import re
+    string = re.sub(r"'s", " ", string)
+    string = re.sub(r"'ve", " have", string)
+    string = re.sub(r"n't", " not", string)
+    string = re.sub(r"'re", " are", string)
+    string = re.sub(r"'d", " ", string)
+    string = re.sub(r"'ll", " ", string)
+    return(string.strip().lower())
+
