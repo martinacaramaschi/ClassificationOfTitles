12ec1880889c7d929dcca1cd18c4a0efffe9f141
martinacaramaschi <martina.caramaschi@studio.unibo.it>
2020-08-05 10:40:32 +0200

    i have created to_tokenize_str, a function that take a sentence and gives the list of contained tokens; to_remove_sw_and_punct_from_list, a function that removes stopwords and punctuations from a list; to_join_list, a function that join a list of tokens, giving a sentence; to_lemmatize_word, a function that lemmatize a word to the closest noun or verb; to_lemmatize, a function that lemmatize all the word into a list of sentence.

--- functions.py
+++ functions.py
@@ -4,9 +4,10 @@
 def print_ciao():
     print("ciao!")
 
-# read_dataset_and_create_df function: it read an excel dataset and returns a dataframe containing the dataset's informations
+# to_read_dataset_and_create_df function: it read an excel dataset 
+# and returns a dataframe containing the dataset's informations   
 # As input the dataset's name (e.g. name_dataset = "name_dataset.xlsx") is needed
-def read_dataset_and_create_df(name_dataset):
+def to_read_dataset_and_create_df(name_dataset):
     import pandas as pd
     # It reads and stores the dataset's information in dataset
     dataset = pd.read_excel(name_dataset, )
@@ -32,10 +33,10 @@ def to_lower(string):
         print("Not valid input! must be a string!")
     return(lower_case)
     
-    
-# clean_str function: it removes or substitutes some string (e.g. 'll is removed and n't become not)
+# to_clean_str function: it removes or substitutes some string 
+# (e.g. 'll is removed and n't become not)
 # As input a string is needed
-def clean_str(string):
+def to_clean_str(string):
     import re
     string = re.sub(r"'s", " ", string)
     string = re.sub(r"'ve", " have", string)
@@ -43,5 +44,71 @@ def clean_str(string):
     string = re.sub(r"'re", " are", string)
     string = re.sub(r"'d", " ", string)
     string = re.sub(r"'ll", " ", string)
+    string = re.sub(r"\n", " ", string)
+    string = re.sub(r"/", " ", string)
+    string = re.sub(r"s'", "s", string)
+    string = re.sub(r"-", "", string)
+    string = re.sub(r"\?", " ", string)
+    string = re.sub(r"\:", "", string)
+    string = re.sub(r"\.", "", string)
     return(string.strip().lower())
 
+# to_tokenize_str function: it tokenize a sentence; 
+# as output, a list of tokens is given
+# As input, a string is needed 
+def to_tokenize_str(string):
+    from nltk.tokenize import word_tokenize
+    word_tokens = []
+    word_tokens = word_tokenize(string)
+    return(word_tokens)
+
+# to_remove_sw_and_punct_from_list function: it removes stopwords and punctuation from a list; 
+# as output, the list of remained tokens is given
+# As input, a list is needed 
+def to_remove_sw_and_punct_from_list(list):
+    from nltk.corpus import stopwords
+    new_list = []
+    sw = set(stopwords.words('english')) 
+    punct = {'.', ':', ',', '!', '?', '--', '``', '-','(', ')', "'", '\n', "''", '&'}
+    for w in list:
+        if w not in sw and w not in punct:
+            new_list.append(w)
+    return(new_list)
+# to_join_list function: it join a list of elements; as output, the sentence is given
+# As input, a list is needed 
+def to_join_list(list):
+    sep = ' '
+    sentence = sep.join(list)
+    return(sentence)
+
+# to_remove_stopw_and_punct function: it removes stopw and punctuations
+# as output, a list of sentences is given
+# As input, a list of sentence is needed 
+def to_remove_stopw_and_punct(titles):
+    filtered_titles = [[] for i in range(len(titles))]
+    for i in range(len(titles)):
+        filtered_titles[i] = to_tokenize_str(titles[i])
+        filtered_titles[i] = to_remove_sw_and_punct_from_list(filtered_titles[i])
+        filtered_titles[i] = to_join_list(filtered_titles[i])
+    return(filtered_titles)
+
+
+def to_lemmatize(titles):
+    lemmatized_tokens = [[] for i in range(len(titles))]
+    lemmatized_titles = [[] for i in range(len(titles))]
+    for i in range(len(titles)):
+        list = []
+        list = to_tokenize_str(titles[i])
+        for token in list:
+            tokenized = to_lemmatize_word(token)
+            lemmatized_tokens[i].append(tokenized)
+        lemmatized_titles[i] = to_join_list(lemmatized_tokens[i])
+    return(lemmatized_titles)
+    
+def to_lemmatize_word(w):
+    from nltk.stem import WordNetLemmatizer
+    lemmatizer = WordNetLemmatizer()
+    lemma = lemmatizer.lemmatize(w)
+    new_w = lemmatizer.lemmatize((lemma), "v")
+    return(new_w)
+            
