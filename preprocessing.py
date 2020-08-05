6263c0999985e51eab52608c62d193083153b203
martinacaramaschi <martina.caramaschi@studio.unibo.it>
2020-08-05 17:10:09 +0200

    i have finished the preprocessing function. now, this function preprocesses a list of sentences, before using it for word2vec's model training.

--- preprocessing.py
+++ preprocessing.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from functions import to_lower , to_clean_str, to_remove_stopw_and_punct
+from functions import to_lower , to_clean_str, to_remove_sw_and_punct_from_sent, \
+                          to_lemmatize_sent
 
-# preprocessing_titles function: it preprocess the titles before the word2vec training
-# As input the list of titles, the lenght of the list are needed
+# preprocessing_titles function:
+# As input, a list of sentences (in my case, titles) is needed.
+# It lowercased all sentences; remove stop words and punctuation symbols from sentences;
+       # it lemmatized all nouns and verbs.
+# As output, the list of senteces after all preprocessing steps.
 def preprocessing_titles(titles):
     # Step to lowercase all titles
     titles_lowercase = []
@@ -16,5 +20,9 @@ def preprocessing_titles(titles):
     # Step to remove stop words and punctuation symbols
     titles_without_sw_and_punct = []
     for line in titles_cleaned:
-        titles_without_sw_and_punct.append(to_remove_stopw_and_punct(line))
-        return(titles_cleaned)
+        titles_without_sw_and_punct.append(to_remove_sw_and_punct_from_sent(line))
+    # Step to lemmatize verbs and nouns
+    titles_lemmatized = []
+    for line in titles_without_sw_and_punct:
+        titles_lemmatized.append(to_lemmatize_sent(line))    
+    return(titles_lemmatized)
