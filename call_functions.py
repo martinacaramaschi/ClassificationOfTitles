1568e2236cda3bfe615d0da317d3dee46fca8f9b
martinacaramaschi <martina.caramaschi@studio.unibo.it>
2020-09-23 15:32:54 +0200

    call_functions updated: it simulates woe to make the classifier

--- call_functions.py
+++ call_functions.py
@@ -1,103 +1,64 @@
 # -*- coding: utf-8 -*-
+"""
+This file called call_functions is the prototipe of what a user should do, step
+by step, to train his/her classifier, having a starting dataset, containing 
+titles and corresponding class which the title belong to.
+"""
+# FIRST we import all necessary fuctions
+#from importing_dataset import to_import_dataset
+#from preprocessing import preprocessing_titles
+from functions import to_import_dataset, preprocessing_titles, from_word_to_vec, \
+                      to_average_all_titles, to_create_classes_vector, \
+                      divide_train_test, classifier
+                      
 
-''' 
-to_lower                             # 2.
-to_read_dataset_and_create_df        # 3.
-to_clean_str                         # 4.
-to_tokenize_str                      # 5.
-to_remove_sw_and_punct_from_list     # 6.
-to_join_list                         # 7.
-to_remove_sw_and_punct_from_sent     # 8.
-to_lemmatize_word                    # 9.
-to_lemmatize_sent                    # 10.
-
-'''
-# from functions import list_words
-from importing_dataset import to_import_dataset
-from preprocessing import preprocessing_titles
-from functions import to_list_all_words_with_repetition, to_list_all_words_no_repetition, \
-                      from_word_to_vec
-
-# importing titles and corresponding classes
+# SECOND we import the titles and corresponding classes
+# we saved them into my_titles and my_classes, two ordered vectors
 my_titles, my_classes = to_import_dataset("database_TitlesAndClusters.xlsx", 0, 1)
-print(my_titles)
-print(my_classes)
+#print(my_titles)
+#print(my_classes)
 
-# Testing preprocessing_titles(titles) function: print the results
+# THIRD we preprocess the titles
+#the output is a vector of tokens, cleaned and uniformed, per each titles
 preprocessed_titles = []
 preprocessed_titles = preprocessing_titles(my_titles) 
+len_titles = len(preprocessed_titles)
 
-# from word to vec
+# FOURTH we create the vectors of real numbers, that represent the words
+# vocabulary contains all words
+# model contains all vector of real numbers per each word
 model, vocabulary = from_word_to_vec(preprocessed_titles)
-vocabulary
-model['lens']
+#vocabulary
+#model['lens']
 
-# Testing all_words_with_repetition(sentences) function
-words_with_repetition = to_list_all_words_with_repetition(preprocessed_titles)
-words_without_repetition = to_list_all_words_no_repetition(words_with_repetition)
-print(words_without_repetition)
-len(words_without_repetition)
+#FIFTH we represents all title as the average of its words
+average_sentence = to_average_all_titles(preprocessed_titles, len_titles, model)
 
+""" forse non mi servono più
+# definisco le variabili delle dimensioni
+# N = average_sentence.shape[0]
+# d = average_sentence.shape[1]
+"""
+# SIXTH we assign to each class an integer, that represents that class during the classification
+vector_of_classes, possible_classes = to_create_classes_vector(my_classes)
 
+# SEVENTH we organize the data for the training and testing of classfier
+train_x, train_y, test_x, test_y = divide_train_test(average_sentence, vector_of_classes)
+print(train_x.shape, train_y.shape, test_x.shape, test_y.shape)
+
+#classificatore
+score_train, score_test = classifier(train_x, train_y, test_x, test_y)
+score_train
+score_test
 
 """
-preprocessed titles è l'input per word2vec
-"""
+from sklearn.neural_network import MLPClassifier
+model_MLP = MLPClassifier(hidden_layer_sizes=(200, 100))
+reg = model_MLP.fit(train_x, train_y)
 
-#test project
-# input_titles = []
-# input_titles = preprocessing_titles(titles)
-# print(list_words(input_titles))
+print(reg.score(train_x, train_y))
+print(reg.score(test_x, test_y))
+print(print(reg.predict_proba(test_x[:10])))
 
-
-
-
-#----------------------------------------------------------------------------#
-"""
-# Testing print_ciao() function
-print_ciao()
-
-# Testing to_lower(string) function
-to_lower("hELLO World!")
-
-# Testing clean_str(string) function
-to_clean_str("i don't know. maybe i'll do?")
-
-# Testing to_tokenize_str(string) function
-print(to_tokenize_str("i love u. and for ever!"))
-
-# Testing to_remove_sw_and_punct_from_list(list) function
-tokens_without_sw = to_remove_sw_and_punct_from_list(to_tokenize_str("i love u and for ever!"))
-
-# Testing to_join_list(list) function
-print(to_join_list(tokens_without_sw))
-
-# New test
-# Testing to_remove_stopw_and_punct_from_sent(sentence) function
-print(to_remove_sw_and_punct_from_sent("I'll be a great physicist! Always, thank you!"))
-
-# Testing to_lemmatize_word function
-print(to_lemmatize_word("friends"))
-print(to_lemmatize_word("playing"))
-
-function works, but is not used
-# Testing to_remove_stopw_and_punct(titles) function
-print(to_remove_stopw_and_punct(titles))
-
-# Testing to_lemmatize_sent(sentence) function
-sent_test2 = "today i'll playing football with my best friends"
-print(to_lemmatize_sent(sent_test2))
-
-#OLD TEST
-# Testing preprocessing_titles(titles) function
-lower_and_cleaned_titles = []
-lower_and_cleaned_titles = preprocessing_titles(titles)
-len(lower_and_cleaned_titles)
-print(lower_and_cleaned_titles)
-
-# Testing preprocessing_titles(titles) function
-preprocessed_titles = []
-preprocessed_titles = preprocessing_titles(titles)
-#len(lower_and_cleaned_and_without_sw_punct_titles)
-print(preprocessed_titles)
+print(reg.predict(test_x[:8]))
 """
